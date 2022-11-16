ff296f533e92a53e3756f751e6e390dcf51e9509
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-11-16 07:47:32 -0800

    new comt

--- shell.c
+++ shell.c
@@ -1,8 +1,4 @@
 #include "shell.h"
-char **commands = NULL;
-char *line = NULL;
-char *shell_name = NULL;
-int status = 0;
 
 /**
  * main - Entry point
@@ -12,6 +8,10 @@ int status = 0;
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
+char **commands = NULL;
+char *line = NULL;
+char *shell_name = NULL;
+int status = 0;
 char **current_command = NULL;
 int i, type_command = 0;
 size_t n = 0;
