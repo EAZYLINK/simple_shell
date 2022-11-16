b6a40727dec772b6b99051ffce98ec72b08a6a1e
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-11-16 18:36:39 +0100

    Update helper_3.c

--- helper_3.c
+++ helper_3.c
@@ -33,28 +33,3 @@ return (string);
 *save_ptr = finish + 1;
 return (string);
 }
-
-/**
-* _atoi - changes string to an integer
-* @s: the string to be cconverted
-* Return: the intger value of string
-*/
-
-int _atoi(char *s)
-{
-unsigned int n = 0;
-do
-{
-if (*s == '_')
-return (-1);
-else if ((*s < '0' || *s > '9')
-&& *s != '\0')
-return (-1);
-else if (*s >= '0' && *s <= '9')
-n = (n * 10) + (*s - '0');
-else if (n > 0)
-break;
-}
-while (*s++);
-return (n);
-}
