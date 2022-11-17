4ddc2477dda6f4b7023d3b756734a66cee71b962
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-11-17 05:58:41 +0100

    Update helper_1.c

--- helper_1.c
+++ helper_1.c
@@ -45,3 +45,22 @@ for (; string[len] != '\0'; len++)
 {}
 return (len);
 }
+
+/**
+ *_strchr - locates a char in a string
+ *@s: string to be searched
+ *@c: char to be checked
+ *
+ *Return: pointer to the first occurence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+for (; s[i] != c && s[i] != '\0'; i++)
+{}
+if (s[i] == c)
+return (s + i);
+else
+return (NULL);
+}
