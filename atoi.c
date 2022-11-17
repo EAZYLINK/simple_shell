100214b8aea966cdf84a5ee3860ef29ae066d520
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-11-17 16:38:46 +0100

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -11,8 +11,7 @@ unsigned int n = 0;
 do {
 if (*s == '-')
 return (-1);
-else if ((*s < '0' || *s > '9')
-&& *s != '\0')
+else if ((*s < '0' || *s > '9') && *s != '\0')
 return (-1);
 else if (*s >= '0' && *s <= '9')
 n = (n * 10) + (*s - '0');
