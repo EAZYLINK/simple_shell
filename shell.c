b7d89ccdc16bfcba105af52e70ff2f0534ac2818
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-11-17 06:01:29 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,10 +1,5 @@
 #include "shell.h"
 
-char **commands = NULL;
-char *line = NULL;
-char *shell_name = NULL;
-int status = 0;
-
 /**
  * main - Entry point
  * @argc: number of arguments parsed
@@ -13,6 +8,10 @@ int status = 0;
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
+char **commands = NULL;
+char *line = NULL;
+char *shell_name = NULL;
+int status = 0;
 char **current_command = NULL;
 int i, type_command = 0;
 size_t n = 0;
