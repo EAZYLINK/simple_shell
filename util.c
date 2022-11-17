75b8d6989df476a677d728e986978d7d65f13f9b
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-11-17 05:22:25 +0100

    Update util.c

--- util.c
+++ util.c
@@ -84,14 +84,14 @@ status = 127;
 char *check_path(char *command)
 {
 char **path_array = NULL;
-char *temp, temp2, *path_cpy;
+char *temp, *temp2, *path_cpy;
 char *path = _getenv("PATH");
 int i;
 if (path == NULL || _strlen(path) == 0)
 return (NULL);
 path_cpy = malloc(sizeof(*path_cpy) * (_strlen(path) + 1));
 _strcpy(path, path_cpy);
-path_array = tokenizer(path_py, ":");
+path_array = tokenizer(path_cpy, ":");
 for (i = 0; path_array[i] != NULL; i++)
 {
 temp2 = _strcat(path_array[i], "/");
