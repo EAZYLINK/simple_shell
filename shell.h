fc4f4462df011fb0e26b292751177c4cc64b1c76
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-11-17 14:20:26 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,9 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -38,21 +38,19 @@ extern char **commands;
 extern char *shell_name;
 extern int status;
 
-/*helpers1*/
+/*helpers*/
 void print(char *, int);
 char **tokenizer(char *, char *);
 void remove_newline(char *);
 int _strlen(char *);
 void _strcpy(char *, char *);
 
-/*helpers2*/
 int _strcmp(char *, char *);
-char *_strcat(char *, char*);
+char *_strcat(char *, char *);
 int _strspn(char *, char *);
 int _strcspn(char *, char *);
 char *_strchr(char *, char);
 
-/*helpers3*/
 char *_strtok_r(char *, char *, char **);
 int _atoi(char *);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
